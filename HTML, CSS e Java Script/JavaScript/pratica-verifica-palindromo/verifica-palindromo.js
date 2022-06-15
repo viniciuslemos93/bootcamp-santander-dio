8812ce532a7506c3a6eeab3453bec092d622869d
Vinicius R. Lemos <vinycius1993@gmail.com>
2022-06-15 09:35:07 -0300

    segunda possibilidade de resolução da verificação do palindromo

--- HTML, CSS e Java Script/JavaScript/pratica-verifica-palindromo/verifica-palindromo.js
+++ HTML, CSS e Java Script/JavaScript/pratica-verifica-palindromo/verifica-palindromo.js
@@ -8,4 +8,22 @@ function verificaPalindromo (string) {
     - O método join() junta todos os elementos de um array (ou um array-like object) em uma string e retorna esta string.
     */
 }
-console.log(verificaPalindromo("ovo"));
+//console.log(verificaPalindromo("ovo"));
+function verificaPalindromo (string) {
+    if (!string) return "String inexistente"
+    
+    for (let i = 0; i < string.length / 2; i++) { //length / 2 pois não precisamos percorrer toda a string, apenas a metade dela.
+        if (string[i] !== string[string.length - 1 - i]) {/*Testando se a string na posição [i] não é igual ao total de caracteres da string - 1 - i
+        // o índice inicia no 0, mas o length inicia no 1. Ex.:
+        palavra: abba
+        índice:  0123 -> .length vai ser 4
+        Neste exemplo vai comparar o indice 0 se não é igual ao indice 3. Após o primeiro loop, comparará o índice 1 com o 2, e assim por diante.
+        */
+
+        return false; // Se a string não for igual, retorna falso.
+        }
+        
+    }
+    return true;// Se a string não for diferente, retorna true.    
+}
+console.log(verificaPalindromo("gato"));
